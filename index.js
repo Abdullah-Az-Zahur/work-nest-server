72cfc6fe1de201d269b064d3c8c8632fac3f5681
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-05-12 00:03:53 +0600

    get single job data

--- index.js
+++ index.js
@@ -42,7 +42,15 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // get single job
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
