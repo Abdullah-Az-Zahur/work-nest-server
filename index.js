966e5dfc639070c2c89b23fc05a543a9cb5dd388
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-05-11 19:48:59 +0600

    get all jobs data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,9 +15,7 @@ app.use(
 );
 app.use(express.json());
 
-const uri =
-//   "mongodb+srv://<username>:<password>@cluster0.nbrjeuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-  "mongodb+srv://workNestUser:EmbTI9rxOpUuXS8u@cluster0.nbrjeuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nbrjeuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,6 +31,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const jobsCollection = client.db("workNestDB").collection("jobs");
+    const bidsCollection = client.db("workNestDB").collection("bids");
+
+    // get all jobs
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
